18c80f03d3fc872be5941d451a66239c9f4e5f61
Ali2010-goiohoi <karomatovali36@gmail.com>
2025-10-17 19:21:04 +0500

    TextStyle

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,346 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles{
+  static const TextStyle h1Bold = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 22,
+    fontWeight: FontWeight.w700 ,
+    color: Colors.black
+  );
+  static const  TextStyle h2SemiBold = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+  static const  TextStyle h3600 = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+  static const  TextStyle h4Bold = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static const  TextStyle h5Bold = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static const  TextStyle h6Regular = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  static const  TextStyle h7Bold = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static const  TextStyle h8Bold = TextStyle(
+    fontFamily: 'OpenSans',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static const TextStyle h9Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 18,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black,
+  );
+  static const TextStyle h10Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black,
+  );
+  static const TextStyle h11SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h12SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h13style600 = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h14SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h15style600 = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h16SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h17Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 18,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h18Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h19Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h20Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black,
+  );
+  static const TextStyle h21SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h22Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 22,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h23Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 18,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h24SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h25SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h26Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h27Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h28Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h29SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h30Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h31Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black,
+  );
+  static const TextStyle h32Regular = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h33Regular = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 13,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h34SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h35SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h36Bold = TextStyle(
+      fontFamily: 'SFPROTEXT',
+      fontSize: 13,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h37Bold = TextStyle(
+      fontFamily: 'SFPROTEXT',
+      fontSize: 13,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h38SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h39SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h40Regular = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h41SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h42SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h43SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h44SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h45Regular = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h46Regular = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h47SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h48Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h49Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h50Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h51Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h52Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 14,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+  static const TextStyle h53Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black,
+  );
+  static const TextStyle h54SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 16,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h55Regular = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w400 ,
+      color: Colors.black
+  );
+  static const TextStyle h56Bold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 12,
+      fontWeight: FontWeight.w600 ,
+      color: Colors.black
+  );
+  static const TextStyle h57SemiBold = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 22,
+      fontWeight: FontWeight.w700 ,
+      color: Colors.black
+  );
+}
